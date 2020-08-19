6effb6a6cbe4c526d43658435418b8a22a6694c2
Aaron-Patel <69919999+Aaron-Patel@users.noreply.github.com>
2020-08-19 14:40:43 -0400

    Hello Java Lab

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/**************************************************************************************
+Hello.java
+***************************************************************************************
+Aaron Patel
+08/19/20
+CMSC 255 and Section 002
+***************************************************************************************/
+public class Hello{
+	public static void main (String[] args) {
+	
+	System.out.println("Aaron Patel");
+	System.out.println("Hello.java");
+	System.out.println("The goal of this lab is to first make sure that you have Java and all related applications installed and ready to go.");
+	System.out.println("Version 7.8.9");
+	System.out.println("CMSC 255 and Section 002");
+	System.out.println();
+	}
+}
